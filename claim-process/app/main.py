078eae81205bb6f688d9e9bd4a23d533d89c2ea7
Francisco Prin <franciscoprin@gmail.com>
2025-01-14 09:50:40 -0600

    Bootstrapping: Creating a dummy hello world claim-process service.

---
+++ claim-process/app/main.py
@@ -0,0 +1,10 @@
+# main.py
+from fastapi import FastAPI
+
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+# Define a route for the root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
